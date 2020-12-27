34114900759268a41167b51ce47e57baca073056
Alan Cordova <info.devalan@gmail.com>
2020-12-27 01:46:51 -0600

    Search images engine files for deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState , useEffect} from 'react';
+import Form from './components/Form';
+import ListImages from './components/ListImages';
 
 function App() {
+  const [search, saveSearch] = useState('');
+  const [images, saveImages] = useState([]);
+  const [currentPage, saveCurrentPage] = useState(1);
+  const [totalPages, savetotalPages] = useState(1);
+    
+    useEffect(() => {
+      const checkApi = async () => {
+            if(search === '') return;
+            const imagesPerPage = 30;
+            const key = '19675010-a05df9ff14559b7dfd363aac8';
+            const url = `https://pixabay.com/api/?key=${key}&q=${search}&per_page=${imagesPerPage}&page=${currentPage}`;
+
+            const response = await fetch(url);
+            const result = await response.json();
+
+            saveImages(result.hits);
+
+            const calculateTotalPages = Math.ceil(result.totalHits / imagesPerPage);
+            savetotalPages(calculateTotalPages);
+
+            const jumbotron = document.querySelector('.jumbotron');
+            jumbotron.scrollIntoView({behavior: 'smooth'});
+        }
+        checkApi();
+    }, [search, currentPage])
+
+    const prevPage = () => {
+      const newCurrentPage = currentPage - 1;
+      if(newCurrentPage === 0) return;
+      saveCurrentPage(newCurrentPage);
+    }
+
+    const nextPage = () => {
+      const newCurrentPage = currentPage + 1;
+      if(newCurrentPage > totalPages) return;
+      saveCurrentPage(newCurrentPage);
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador de Imágenes</p>
+        <Form saveSearch={saveSearch} />
+      </div>
+      <div className="row justify-content-center">
+        <ListImages images={images} />
+        {
+        (currentPage === 1)?null:(<button type="button" className="btn btn-info mr-1" onClick={prevPage}>&laquo;Anterior</button>)
+        }
+        {
+        (currentPage === totalPages)?null:(<button type="button" className="btn btn-info mr-1" onClick={nextPage}>Siguiente&raquo;</button>)
+        }
+      </div>
     </div>
   );
 }
